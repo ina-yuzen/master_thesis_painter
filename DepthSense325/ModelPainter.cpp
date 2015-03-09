4522c32ef09e170491c29bcb29c65fcc7b15b43b
ina_yuzen <leonhard.mch.chatte@gmail.com>
2015-03-09 17:11:49 +0900

    Color clicked point, not polygon

--- DepthSense325/ModelPainter.cpp
+++ DepthSense325/ModelPainter.cpp
@@ -18,12 +18,66 @@ ModelPainter::ModelPainter(Polycode::Scene *scene, Polycode::SceneMesh *mesh) :
 	input->addEventListener(this, InputEvent::EVENT_MOUSEUP);
 }
 
-static int FindIntersectionPolygon(Polycode::SceneMesh *mesh, Polycode::Ray ray) {
+// http://geomalgorithms.com/a06-_intersect-2.html#intersect3D_RayTriangle()
+struct Intersection {
+	bool found;
+	int first_vertex_index;
+	double s, t; // intersection point is s(v1-v0) + t(v2-v0)
+	Polycode::Vector3 point;
+};
+
+const double kEpsilon = 0.000001;
+
+static Intersection CalculateIntersectionPoint(const Polycode::Ray& ray, 
+											   const Polycode::Vector3& v0,
+											   const Polycode::Vector3& v1,
+											   const Polycode::Vector3& v2) 
+{
+	Intersection res;
+	res.found = false;
+	auto u = v1 - v0, v = v2 - v0;
+	auto normal = u.crossProduct(v);
+	if (normal.length() < kEpsilon) {
+		return res;
+	}
+	auto w0 = ray.origin - v0;
+	auto a = - normal.dot(w0);
+	auto b = normal.dot(ray.direction);
+	if (fabs(b) < kEpsilon) {
+		return res;
+	}
+	auto r = a / b;
+	if (r < 0.0) {
+		return res;
+	}
+	auto intersection = ray.origin + ray.direction * r;
+
+	auto uu = u.dot(u), uv = u.dot(v), vv = v.dot(v);
+	auto w = intersection - v0;
+	auto wu = w.dot(u), wv = w.dot(v);
+	auto d = uv * uv - uu * vv;
+
+	auto s = (uv * wv - vv * wu) / d;
+	if (s < 0.0 || s > 1.0)
+		return res;
+	auto t = (uv * wu - uu * wv) / d;
+	if (t < 0.0 || (s + t) > 1.0)
+		return res;
+	res.found = true;
+	res.point = intersection;
+	res.s = s;
+	res.t = t;
+	return res;
+}
+
+static Intersection FindIntersectionPolygon(Polycode::SceneMesh *mesh, const Polycode::Ray& ray) {
 	auto raw = mesh->getMesh();
-	int vector_idx = -1;
+	Intersection best;
+	best.found = false;
 	double distance = 1e10;
 
 	std::vector<Polycode::Vector3> adjusted_points;
+	auto mesh_transform = mesh->getTransformMatrix();
 	for (int vidx=0; vidx < raw->vertexPositionArray.data.size()/3; vidx ++) {
 		auto rest_vert = Polycode::Vector3(raw->vertexPositionArray.data[vidx*3],
 			raw->vertexPositionArray.data[vidx*3+1],
@@ -38,7 +92,7 @@ static int FindIntersectionPolygon(Polycode::SceneMesh *mesh, Polycode::Ray ray)
 				real_pos += bone->finalMatrix * rest_vert * weight;
 			}
 		}
-		adjusted_points.push_back(real_pos);
+		adjusted_points.push_back(mesh_transform * real_pos);
 	}
 
 	int step = raw->getIndexGroupSize();
@@ -46,47 +100,57 @@ static int FindIntersectionPolygon(Polycode::SceneMesh *mesh, Polycode::Ray ray)
 		auto idx0 = raw->indexArray.data[ii],
 			idx1 = raw->indexArray.data[ii+1],
 			idx2 = raw->indexArray.data[ii+2];
-		if(ray.polygonIntersect(adjusted_points[idx0], adjusted_points[idx1], adjusted_points[idx2])) {
-			auto this_dist = adjusted_points[idx0].distance(ray.origin);
+		auto res = CalculateIntersectionPoint(ray, adjusted_points[idx0], adjusted_points[idx1], adjusted_points[idx2]);
+		if (res.found) {
+			res.first_vertex_index = ii;
+			auto this_dist = res.point.distance(ray.origin);
 			if (distance > this_dist) {
-				vector_idx = ii;
+				best = res;
 				distance = this_dist;
 			}
 		}
 	}
-	return vector_idx;
+	return best;
 }
 
 typedef std::pair<int ,int> TP;
-static int Interpolate(TP start, TP end, int y) {
+static int Interpolate(const TP& start, const TP& end, int y) {
 	double ratio = (y - start.second) / static_cast<double>(end.second - start.second);
 	return start.first * (1 - ratio) + end.first * ratio;
 }
 
-static void FillTexture(Polycode::SceneMesh *mesh, int iidx, Polycode::Color color) {
-	std::vector<TP> region;
+const double kPenSize = 500;
+
+static void FillTexture(Polycode::SceneMesh *mesh, const Intersection& intersection, const Polycode::Color& color) {
+	auto raw = mesh->getMesh();
+	auto idx0 = intersection.first_vertex_index;
+	auto tc0 = raw->getVertexTexCoordAtIndex(idx0);
+	auto tc1 = raw->getVertexTexCoordAtIndex(idx0+1);
+	auto tc2 = raw->getVertexTexCoordAtIndex(idx0+2);
+	auto v0 = raw->getVertexPositionAtIndex(idx0);
+	auto v1 = raw->getVertexPositionAtIndex(idx0+1);
+	auto v2 = raw->getVertexPositionAtIndex(idx0+2);
+	auto tc = (tc1 - tc0) * intersection.s + (tc2 - tc0) * intersection.t + tc0;
+
+	auto carea = (tc1-tc0).crossProduct(tc2-tc0);
+	auto varea = (v1-v0).crossProduct(v2-v0).length();
+	int normalized_pen_size = kPenSize * carea / varea;
+
 	auto texture = mesh->getTexture();
 	auto height = texture->getHeight(), width = texture->getWidth();
-	for (int off = 0; off < 3; off ++) {
-		auto coord = mesh->getMesh()->getVertexTexCoord(mesh->getMesh()->indexArray.data[iidx + off]);
-		region.push_back(TP(static_cast<int>(width * coord.x), static_cast<int>(height * coord.y)));
-	}
-	std::sort(region.begin(), region.end(), [](TP l, TP r) { return l.second < r.second; });
+
 	auto buffer = texture->getTextureData();
-	for (int y = region[0].second; y <= region[2].second; y ++) {
-		int x1= Interpolate(region[0], region[2], y),
-			x2;
-		if (y < region[1].second || region[1].second == region[2].second) {
-			x2 = Interpolate(region[0], region[1], y);
-		} else {
-			x2 = Interpolate(region[1], region[2], y);
-		}
-		if (x1 > x2) {
-			std::swap(x1, x2);
-		}
-		for (int x = x1; x < x2; x ++) {
+	unsigned int rgba = color.getUint();
+	auto cx = width * tc.x, cy = height * tc.y;
+	for (int dy = -normalized_pen_size; dy <= normalized_pen_size; dy ++) {
+		int y = cy + dy;
+		if (y < 0 || y >= height)
+			continue;
+		for (int dx = - normalized_pen_size + abs(dy); dx <= normalized_pen_size - abs(dy); dx++) {
+			int x = cx + dx;
+			if (x < 0 || x >= width)
+				continue;
 			int i = y * width + x;
-			unsigned int rgba = color.getUint();
 			buffer[i * 4    ] = rgba & 0xff;
 			buffer[i * 4 + 1] = (rgba >> 8) & 0xff;
 			buffer[i * 4 + 2] = (rgba >> 16) & 0xff;
@@ -105,10 +169,10 @@ void ModelPainter::handleEvent(Polycode::Event *e) {
 		auto ray = scene_->projectRayFromCameraAndViewportCoordinate(scene_->getActiveCamera(), ie->getMousePosition());
 		auto raw = mesh_->getMesh();
 		assert(raw->getMeshType() == Polycode::Mesh::TRI_MESH);
-		auto idx = FindIntersectionPolygon(mesh_, ray);
-		if (idx == -1)
-			return;
-		FillTexture(mesh_, idx, picker_->current_color());
+		auto intersection = FindIntersectionPolygon(mesh_, ray);
+		if (intersection.found) {
+			FillTexture(mesh_, intersection, picker_->current_color());
+		}
 	};
 
 	auto set_click_state = [&](bool new_val) {
