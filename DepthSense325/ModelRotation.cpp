c89ab8c31baab8df7e6db649a60f80a66d067493
ina-yuzen <leonhard.mch.chatte@gmail.com>
2015-03-22 02:56:30 +0900

    Rotate model by 2 fingers touch

--- DepthSense325/ModelRotation.cpp
+++ DepthSense325/ModelRotation.cpp
@@ -12,9 +12,9 @@ ModelRotation::ModelRotation(Polycode::SceneMesh *mesh):
 {
 	auto input = Polycode::CoreServices::getInstance()->getInput();
 	using Polycode::InputEvent;
-	input->addEventListener(this, InputEvent::EVENT_MOUSEMOVE);
-	input->addEventListener(this, InputEvent::EVENT_MOUSEDOWN);
-	input->addEventListener(this, InputEvent::EVENT_MOUSEUP);
+	input->addEventListener(this, InputEvent::EVENT_TOUCHES_BEGAN);
+	input->addEventListener(this, InputEvent::EVENT_TOUCHES_ENDED);
+	input->addEventListener(this, InputEvent::EVENT_TOUCHES_MOVED);
 }
 
 const int kMouseRightButtonCode = 1;
@@ -24,26 +24,29 @@ void ModelRotation::handleEvent(Polycode::Event *e) {
 
 	auto set_click_state = [&](bool new_val) {
 		InputEvent *ie = (InputEvent*)e;
-		if (ie->mouseButton == kMouseRightButtonCode) {
+		if (new_val && ie->touches.size() == 2) {
 			moving_ = new_val;
-			mouse_prev_ = ie->mousePosition;
+			mouse_prev_ = ie->touches.front().position;
+		}
+		else if (moving_ == true && !new_val) {
+			moving_ = new_val; 
 		}
 	};
 
 	switch (e->getEventCode()) {
-	case InputEvent::EVENT_MOUSEMOVE:
+	case InputEvent::EVENT_TOUCHES_MOVED:
 		if (moving_) {
-			auto new_pos = ((InputEvent*)e)->mousePosition;
+			auto new_pos = ((InputEvent*)e)->touches.front().position;
 			auto diff = new_pos - mouse_prev_;
 			mesh_->Yaw(diff.x * kSensitivity);
 			mesh_->Pitch(diff.y * kSensitivity);
 			mouse_prev_ = new_pos;
 		}
 		break;
-	case InputEvent::EVENT_MOUSEDOWN:
+	case InputEvent::EVENT_TOUCHES_BEGAN:
 		set_click_state(true);
 		break;
-	case InputEvent::EVENT_MOUSEUP:
+	case InputEvent::EVENT_TOUCHES_ENDED:
 		set_click_state(false);
 		break;
 	}
