8e03f331d58645493cb74f1773fc013c136608bc
ina-yuzen <leonhard.mch.chatte@gmail.com>
2015-03-27 00:12:14 +0900

    Detect by right edge

--- DepthSense325/Algorithms.h
+++ DepthSense325/Algorithms.h
@@ -10,6 +10,7 @@ struct Context;
 struct DepthMap;
 std::vector<cv::Point> PinchErosion(std::shared_ptr<Context> context, const DepthMap& data);
 std::vector<cv::Point> PinchNailColor(std::shared_ptr<Context> context, const DepthMap& data);
+std::vector<cv::Point> PinchRightEdge(std::shared_ptr<Context> context, const DepthMap& data);
 cv::Vec3i DetectTouch(const DepthMap& data);
 
 void NotifyPinchChange(std::shared_ptr<Context> context, const DepthMap& depth_map, const std::vector<cv::Point>& founds);
