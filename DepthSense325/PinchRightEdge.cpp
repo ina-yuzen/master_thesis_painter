85697429b8313b71ce05e04dbfa62cdfafb77d1f
ina_yuzen <leonhard.mch.chatte@gmail.com>
2015-04-10 22:19:07 +0900

    Detect Z

--- DepthSense325/PinchRightEdge.cpp
+++ DepthSense325/PinchRightEdge.cpp
@@ -19,7 +19,18 @@ static bool HasHandHole(CvSeq *parent) {
 	return false;
 }
 
+static CvSeq* FindLargeHole(CvSeq *parent) {
+	auto hole = parent->v_next;
+	while (hole) {
+		if (cvContourArea(hole) > kMinHoleSize)
+			return hole;
+		hole = hole->h_next;
+	}
+	return NULL;
+}
+
 Option<cv::Point3f> PinchRightEdge(std::shared_ptr<Context> context, const DepthMap& data) {
+	assert(!data.raw_mat.empty());
 	assert(!data.binary.empty());
 	CvMemStorage *storage = cvCreateMemStorage(0);
 	CvSeq *cSeq = NULL;
@@ -31,20 +42,32 @@ Option<cv::Point3f> PinchRightEdge(std::shared_ptr<Context> context, const Depth
 		if (cvContourArea(cSeq) > max_area) {
 			found.Clear();
 			// hand with pinch hole
-			if (HasHandHole(cSeq)) {
+			if (CvSeq* hole = FindLargeHole(cSeq)) {
 				CvSeqReader reader;
+				cvStartReadSeq(hole, &reader, 0);
+				double max_y = 0;
+				double min_y = DBL_MAX;
+				for (int i = 0; i < hole->total; i++) {
+					cv::Point pt;
+					CV_READ_SEQ_ELEM(pt, reader);
+					if (pt.y > max_y) max_y = pt.y;
+					if (pt.y < min_y) min_y = pt.y;
+				}
 				cvStartReadSeq(cSeq, &reader, 0);
-				cv::Point right_most_point(0,0);
+				cv::Point3f right_most_point(0,0,0);
 				for (int i = 0; i < cSeq->total; i++) {
 					cv::Point pt;
 					CV_READ_SEQ_ELEM(pt, reader);
-					if (right_most_point.x < pt.x)
-						right_most_point = pt;
+					if (right_most_point.x < pt.x && pt.y >= min_y && pt.y <= max_y) {
+						right_most_point.x = pt.x;
+						right_most_point.y = pt.y;
+						right_most_point.z = data.raw_mat.at<uint16_t>(pt.y, pt.x);
+					}
 				}
 				found.Reset(cv::Point3f(
 					right_most_point.x / static_cast<float>(data.w),
 					right_most_point.y / static_cast<float>(data.h),
-					100)); // TOOD: estimate depth from depthmap
+					right_most_point.z)); // TOOD: estimate depth from depthmap
 			}
 		}
 		cSeq = cSeq->h_next;
